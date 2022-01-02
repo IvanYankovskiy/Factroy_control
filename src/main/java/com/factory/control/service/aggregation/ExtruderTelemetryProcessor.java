f62c67e185ebe63319c7d01a67a59c09965b7a1b
IvanYankovskiy <igyankovskiy@gmail.com>
2022-01-02 17:59:03 +0700

    Preserve zeroes during aggregation

--- src/main/java/com/factory/control/service/aggregation/ExtruderTelemetryProcessor.java
+++ src/main/java/com/factory/control/service/aggregation/ExtruderTelemetryProcessor.java
@@ -4,7 +4,7 @@ import com.factory.control.domain.bo.Temp;
 import com.factory.control.domain.entities.ExtruderTelemetry;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,16 +13,35 @@ public class ExtruderTelemetryProcessor implements TimeBasedProcessor<ExtruderTe
 
     @Override
     public List<ExtruderTelemetry> reduce(List<ExtruderTelemetry> input) {
+        if (input.isEmpty()) return new ArrayList<>();
+
         input.sort(Comparator.comparing(Temp::getTime));
         // TODO: implement grouping by Device
-        Integer total = input.stream().reduce(0, (counter, item) -> counter + item.getCounter(), Integer::sum);
-        ExtruderTelemetry latestItem = input.get(input.size() - 1);
+        List<ExtruderTelemetry> result = new ArrayList<>();
+        ExtruderTelemetry aggregatedTm = null;
+        for (ExtruderTelemetry tm : input) {
+            if (tm.getCounter() == 0) {
+                if (aggregatedTm != null) {
+                    result.add(aggregatedTm);
+                    aggregatedTm = null;
+                }
+                ExtruderTelemetry idleTm = new ExtruderTelemetry();
+                idleTm.setCounter(0);
+                idleTm.setTime(tm.getTime());
+                idleTm.setDevice(tm.getDevice());
+                result.add(idleTm);
+            } else {
+                if (aggregatedTm == null) {
+                    aggregatedTm = new ExtruderTelemetry();
+                    aggregatedTm.setCounter(0);
+                }
+                aggregatedTm.setCounter(aggregatedTm.getCounter() + tm.getCounter());
+                aggregatedTm.setTime(tm.getTime());
+                aggregatedTm.setDevice(tm.getDevice());
+            }
+        }
+        if (aggregatedTm != null) result.add(aggregatedTm);
 
-        return Collections.singletonList(
-                new ExtruderTelemetry()
-                        .setCounter(total)
-                        .setTime(latestItem.getTime())
-                        .setDevice(latestItem.getDevice())
-        );
+        return result;
     }
 }
--- src/test/java/com/factory/control/service/aggregation/ExtruderTelemetryProcessorTest.java
+++ src/test/java/com/factory/control/service/aggregation/ExtruderTelemetryProcessorTest.java
@@ -1,6 +1,7 @@
 package com.factory.control.service.aggregation;
 
 import com.factory.control.domain.entities.ExtruderTelemetry;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.OffsetDateTime;
@@ -14,9 +15,10 @@ class ExtruderTelemetryProcessorTest {
     private ExtruderTelemetryProcessor aggregator = new ExtruderTelemetryProcessor();
 
     @Test
+    @DisplayName("Reduce unsorted telemetry without zeroes")
     void testReduceExtruderTelemetry() {
         // given
-        List<ExtruderTelemetry> extruderTelemetryList = createExtruderTelemetryList();
+        List<ExtruderTelemetry> extruderTelemetryList = createExtruderTelemetryListForUnsortedWithoutZeroes();
 
         // when
         List<ExtruderTelemetry> actual = aggregator.reduce(extruderTelemetryList);
@@ -25,7 +27,7 @@ class ExtruderTelemetryProcessorTest {
         assertThat(actual).hasSize(1);
         assertThat(actual.get(0))
                 .usingRecursiveComparison()
-                .ignoringFields("id", "device", "diameter", "density")
+                .ignoringFields("id", "device")
                 .isEqualTo(
                         new ExtruderTelemetry()
                                 .setCounter(40)
@@ -33,7 +35,7 @@ class ExtruderTelemetryProcessorTest {
                 );
     }
 
-    private List<ExtruderTelemetry> createExtruderTelemetryList() {
+    private List<ExtruderTelemetry> createExtruderTelemetryListForUnsortedWithoutZeroes() {
         return Arrays.asList(
                 new ExtruderTelemetry()
                         .setTime(OffsetDateTime.parse("2020-10-21T23:00:17.245+00:00"))
@@ -47,4 +49,98 @@ class ExtruderTelemetryProcessorTest {
         );
     }
 
+    @Test
+    @DisplayName("Reduce unsorted telemetry with zeroes")
+    void testReduceUnsortedWithZeroes() {
+        // given
+        List<ExtruderTelemetry> extruderTelemetryList = createExtruderTelemetryListForUnsortedWithZeroes();
+
+        // when
+        List<ExtruderTelemetry> actual = aggregator.reduce(extruderTelemetryList);
+
+        // then
+        assertThat(actual).hasSize(4);
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "device")
+                .isEqualTo(
+                        List.of(
+                                new ExtruderTelemetry()
+                                        .setCounter(0)
+                                        .setTime(OffsetDateTime.parse("2020-10-21T22:59:07.130+00:00")),
+                                new ExtruderTelemetry()
+                                        .setCounter(20)
+                                        .setTime(OffsetDateTime.parse("2020-10-21T23:01:22.245+00:00")),
+                                new ExtruderTelemetry()
+                                        .setCounter(0)
+                                        .setTime(OffsetDateTime.parse("2020-10-21T23:01:55.155+00:00")),
+                                new ExtruderTelemetry()
+                                        .setCounter(30)
+                                        .setTime(OffsetDateTime.parse("2020-10-21T23:02:07.245+00:00"))
+                        )
+
+                );
+    }
+
+    private List<ExtruderTelemetry> createExtruderTelemetryListForUnsortedWithZeroes() {
+        return Arrays.asList(
+                new ExtruderTelemetry()
+                        .setTime(OffsetDateTime.parse("2020-10-21T23:01:55.155+00:00")) // 3 <-- 0
+                        .setCounter(0),
+                new ExtruderTelemetry()
+                        .setTime(OffsetDateTime.parse("2020-10-21T23:00:17.245+00:00")) // 2.1
+                        .setCounter(10),
+                new ExtruderTelemetry()
+                        .setTime(OffsetDateTime.parse("2020-10-21T23:02:07.245+00:00")) // 4
+                        .setCounter(30),
+                new ExtruderTelemetry()
+                        .setTime(OffsetDateTime.parse("2020-10-21T23:01:22.245+00:00")) // 2.2
+                        .setCounter(10),
+                new ExtruderTelemetry()
+                        .setTime(OffsetDateTime.parse("2020-10-21T22:59:07.130+00:00")) // 1 <-- 0
+                        .setCounter(0)
+        );
+    }
+
+    @Test
+    void testReduceEmptyList() {
+        // when
+        List<ExtruderTelemetry> actual = aggregator.reduce(List.of());
+
+        // then
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void testReduceListWithZeroInTheEnd() {
+        // when
+        List<ExtruderTelemetry> actual = createExtruderTelemetryListWithZeroInTheEnd();
+
+        // then
+        assertThat(actual).hasSize(2);
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "device")
+                .isEqualTo(
+                        List.of(
+                                new ExtruderTelemetry()
+                                        .setCounter(10)
+                                        .setTime(OffsetDateTime.parse("2020-10-21T23:00:17.245+00:00")),
+                                new ExtruderTelemetry()
+                                        .setCounter(0)
+                                        .setTime(OffsetDateTime.parse("2020-10-21T23:02:07.245+00:00"))
+                        )
+                );
+    }
+
+    private List<ExtruderTelemetry> createExtruderTelemetryListWithZeroInTheEnd() {
+        return Arrays.asList(
+                new ExtruderTelemetry()
+                        .setTime(OffsetDateTime.parse("2020-10-21T23:00:17.245+00:00"))
+                        .setCounter(10),
+                new ExtruderTelemetry()
+                        .setTime(OffsetDateTime.parse("2020-10-21T23:02:07.245+00:00"))
+                        .setCounter(0)
+        );
+    }
 }
