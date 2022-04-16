b6643e9058fa6536abb7a1c23862beb2efbf7341
IvanYankovskiy <igyankovskiy@gmail.com>
2022-04-16 17:40:11 +0700

    Add CORS mapping

--- src/main/java/com/factory/control/config/WebConfig.java
+++ src/main/java/com/factory/control/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.factory.control.controller.converter.StringToChronoUnitConverter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,4 +18,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addConverter(new StringToChronoUnitConverter());
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
